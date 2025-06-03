2340861485782949bd50aaf3d88b2c87df6e286f
Ayush Dhingra <ayush.dhingra@luminos.software>
2025-06-03 13:26:32 +0530

    union/function args

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,25 @@
 "use strict";
-//1st Way
+//======1st Way
 let a = "Ayush";
 let b = 25;
 let check;
 check = false;
 // console.log("A", a);
 // console.log("B", b);
-//2nd way
+//=======2nd way
 let fName = "Ayush";
 let age = 24;
-console.log("fName", fName);
-console.log("age", age);
+// console.log("fName", fName);
+// console.log("age", age);
+//========Union type
+let surname;
+let myName = "Ayush";
+surname = 22;
+// console.log("surname", surname);
+// console.log("myName", myName);
+//=========Function arguments
+const func = (n, m) => {
+    console.log("Total", n + m);
+    return n + m;
+};
+func(33, 22);
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-//1st Way
+//======1st Way
 let a: string = "Ayush";
 let b: number = 25;
 let check: boolean;
@@ -7,8 +7,25 @@ check = false;
 // console.log("A", a);
 // console.log("B", b);
 
-//2nd way
+//=======2nd way
 let fName = <string>"Ayush";
 let age = <number>24;
-console.log("fName", fName);
-console.log("age", age);
+// console.log("fName", fName);
+// console.log("age", age);
+
+//========Union type
+
+let surname: string | number;
+let myName = <string | number>"Ayush";
+surname = 22;
+// console.log("surname", surname);
+// console.log("myName", myName);
+
+//=========Function arguments
+
+const func = (n: number, m: number): number => {
+  console.log("Total", n + m);
+  return n + m;
+};
+
+func(33, 22);
