9855bc1fbf95e9ceaf0af8dcf4c2b62cfa37cb03
Ayush Dhingra <ayush.dhingra@luminos.software>
2025-06-03 13:17:28 +0530

    basic datatypes

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,13 @@
 "use strict";
-const a = "Ayush";
-alert(a);
+//1st Way
+let a = "Ayush";
+let b = 25;
+let check;
+check = false;
+// console.log("A", a);
+// console.log("B", b);
+//2nd way
+let fName = "Ayush";
+let age = 24;
+console.log("fName", fName);
+console.log("age", age);
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-const a="Ayush"
-alert(a)
+//1st Way
+let a: string = "Ayush";
+let b: number = 25;
+let check: boolean;
+check = false;
+
+// console.log("A", a);
+// console.log("B", b);
+
+//2nd way
+let fName = <string>"Ayush";
+let age = <number>24;
+console.log("fName", fName);
+console.log("age", age);
